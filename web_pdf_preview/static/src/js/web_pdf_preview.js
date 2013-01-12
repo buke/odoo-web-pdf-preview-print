da2c789fd4e03ba981b629cc5e54ef6f7df3def3
buke <wangbuke@gmail.com>
2013-01-12 14:48:10 +0800

    modified:   web_pdf_preview/__init__.py     modified:   web_pdf_preview/__openerp__.py     modified:   web_pdf_preview/controllers/__init__.py     modified:   web_pdf_preview/controllers/main.py     modified:   web_pdf_preview/static/src/js/web_pdf_preview.js

--- web_pdf_preview/static/src/js/web_pdf_preview.js
+++ web_pdf_preview/static/src/js/web_pdf_preview.js
@@ -15,9 +15,6 @@
 #    You should have received a copy of the GNU Affero General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
-#    For Commercial or OEM, you need to contact the author and/or licensor
-#    and ask for their permission.
-#
 ############################################################################*/
 
 openerp.web_pdf_preview = function(openerp) {
