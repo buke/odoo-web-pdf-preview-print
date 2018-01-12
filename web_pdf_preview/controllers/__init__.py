9b8ea575ae8bc5d94ed8033c3fc1178250e7bc23
kenxin <020cui@163.com>
2018-01-12 16:08:18 +0800

    Update __init__.py

--- web_pdf_preview/controllers/__init__.py
+++ web_pdf_preview/controllers/__init__.py
@@ -17,29 +17,4 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
-
-from openerp.addons.web.controllers.main import Reports, serialize_exception
-from openerp.addons.report.controllers.main import ReportController
-import urllib
-from openerp.addons.web.http import Controller, route, request
-
-class WebPdfReports(ReportController):
-
-    @route(['/report/preview'], type='http', auth="user")
-    def report_download(self, data, token):
-        result = super(WebPdfReports, self).report_download(data, token)
-        result.headers['Content-Disposition'] = result.headers['Content-Disposition'].replace('attachment', 'inline')
-        return result
-
-
-class PreviewReports(Reports):
-
-    @route('/web/report', type='http', auth="user")
-    @serialize_exception
-    def index(self, action, token):
-        result = super(PreviewReports, self).index(action, token)
-        result.headers['Content-Disposition'] = result.headers['Content-Disposition'].replace('attachment', 'inline')
-        return result
-
-
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
+from . import main
